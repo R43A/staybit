ccf4e09f110dc60cbfcc3da8f73619452ae23e88
Raza <ayan6181.am@gmail.com>
2024-05-17 14:36:29 +0530

    Updated

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -35,6 +35,28 @@ function HomePage() {
             </div>
           </Link>
 
+          <button
+            className="navbar-toggler custom-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            style={{
+              backgroundImage:
+                "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAdhAAAHYQGVw7i2AAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAAEtJREFUOI1jZGBgCGNgYFBkIAweMjAwrMAmcYmBgeE/EfgKEZaMAnLBFQbiYgFrzDAyMDDEMDAw6FBg+SigFSA2aq8w4jCA2Ki9AgAa5SmSDueuuwAAAABJRU5ErkJggg==')",
+
+              height: "16px", // Adjust height as needed
+              backgroundSize: "cover",
+              border: "none",
+              borderRadius: "revert",
+              boxShadow: "none",
+            }}
+          >
+            {/* No need for content inside the button */}
+          </button>
+
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
